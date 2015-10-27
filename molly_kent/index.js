31d59152872508fadf073ac2ca0995c72faaa8bf
Molly Kent <kent.molly@gmail.com>
2015-10-26 18:31:14 -0700

    Code cleanup

--- molly_kent/index.js
+++ molly_kent/index.js
@@ -4,6 +4,5 @@ var greet = require(__dirname + '/lib/greet');
 var greet2 = require(__dirname + '/lib/greet2');
 var greet3 = require(__dirname + '/lib/greet3');
 
-console.log(greet(process.argv[2]));
-console.log(greet2.greet(process.argv[2]));
+console.log(greet());
 console.log(greet2.greet(process.argv[2]));
--- molly_kent/lib/greet3.js
+++ molly_kent/lib/greet3.js
@@ -6,5 +6,7 @@ var greet = exports = module.exports = function() {
 
 console.log(greet());
 // this will fire off when it gets required, so the console.log shows up in Node
-// it also gets stored in the variable greet so it can be modified later
-// HANDY for constructor functions!
+
+// the function also gets stored in the variable greet so it can be modified later
+
+// HANDY for constructor functions and adding methods on the prototype down the line
